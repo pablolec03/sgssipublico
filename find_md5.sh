64f08c516ffbefb03a6e2df3b7d439207f0c3b57
pablolec03 <147559876+pablolec03@users.noreply.github.com>
2023-10-18 08:43:10 +0200

    Update find_md5.sh

--- find_md5.sh
+++ find_md5.sh
@@ -1,6 +1,8 @@
-cd md5_folder
+unzip md5.zip -d md5unzip
+
+cd md5unzip
 for file in *.txt; do
-   if echo "9e1cda999b728d2cd7c5ddb46a662522  $file" | md5sum -c --quiet; then
+   if echo "hash  $file" | md5sum -c --quiet; then
        echo "El archivo $file tiene el hash MD5 coincidente."
        cat $file
    fi
